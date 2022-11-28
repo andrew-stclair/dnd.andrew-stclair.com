454f89b16779978bd5fd654772f2a34b875c64ef
Andrew St Clair <andrew_stclair95@hotmail.com>
2022-11-28 15:28:56 +1100

    figures copythrough

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,15 +1,16 @@
 const directoryOutputPlugin = require("@11ty/eleventy-plugin-directory-output");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.setQuietMode(true);
-  eleventyConfig.addPlugin(directoryOutputPlugin, {
-    // Customize columns
-    columns: {
-      filesize: true, // Use `false` to disable
-      benchmark: true, // Use `false` to disable
-    },
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/figures");
+    eleventyConfig.setQuietMode(true);
+    eleventyConfig.addPlugin(directoryOutputPlugin, {
+        // Customize columns
+        columns: {
+            filesize: true, // Use `false` to disable
+            benchmark: true, // Use `false` to disable
+        },
 
-    // Will show in yellow if greater than this number of bytes
-    warningFileSize: 400 * 1000,
-  });
+        // Will show in yellow if greater than this number of bytes
+        warningFileSize: 400 * 1000,
+    });
 };
